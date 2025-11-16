13e603ac2f1c95cb18e594700190ddcdbb0497d3
shmuellevy <shmuellevy16@gmail.com>
2025-11-17 00:08:26 +0200

    Wire AuthProvider and protect routes with ProtectedRoute component

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import { LoginSignup } from './pages/LoginSignup.jsx'
-import { Dashboard } from './pages/Dashboard.jsx'
-import { Onboarding } from './pages/Onboarding.jsx'
-import { UserMsg } from './cmps/UserMsg.jsx'
+import { LoginSignup } from "./pages/LoginSignup.jsx";
+import { Dashboard } from "./pages/Dashboard.jsx";
+import { Onboarding } from "./pages/Onboarding.jsx";
+import { UserMsg } from "./cmps/UserMsg.jsx";
+import { ProtectedRoute } from "./cmps/ProtectedRoute.jsx";
 
 export function RootCmp() {
-    return (
-        <>
-            <UserMsg />
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="/login" element={<LoginSignup />} />
-                <Route path="/onboarding" element={<Onboarding />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <UserMsg />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route
+          path="/onboarding"
+          element={
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
-
-
--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-
 import { store } from './store/store'
+import { AuthProvider } from './contexts/AuthContext'
 import { RootCmp } from './RootCmp'
 
 import './assets/styles/main.scss'
@@ -13,8 +13,10 @@ import './assets/styles/main.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<Provider store={store}>
-		<Router>
-			<RootCmp />
-		</Router>
+		<AuthProvider>
+			<Router>
+				<RootCmp />
+			</Router>
+		</AuthProvider>
 	</Provider>
 )
