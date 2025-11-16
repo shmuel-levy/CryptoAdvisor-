3e7fda6dc084e0d1a3ef022297be45fe8c196f73
shmuellevy <shmuellevy16@gmail.com>
2025-11-16 20:18:23 +0200

    Create a minimal src/assets/styles/main.scss file so the styles import works.

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,6 +1,5 @@
-const { DEV, VITE_LOCAL } = import.meta.env
+const { DEV } = import.meta.env
 
-import { userService as local } from './user.service.local'
 import { userService as remote } from './user.service.remote'
 
 function getEmptyUser() {
@@ -16,8 +15,7 @@ function getEmptyUser() {
     }
 }
 
-const service = VITE_LOCAL === 'true' ? local : remote
-export const userService = { ...service, getEmptyUser }
+export const userService = { ...remote, getEmptyUser }
 
 // Easy access to this service from the dev tools console
 // when using script - dev / dev:local
